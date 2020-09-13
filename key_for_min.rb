bb0fd98c93333149f6f5436eaae52016a4915d85
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-13 18:09:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,14 @@
 def key_for_min_value(name_hash)
   lowest_number = ""
   name_hash.each do |name, number|
-    if lowest_number = ""
+    if lowest_number == ""
       lowest_number = number
     if number < lowest_number
       lowest_number = number
     end
-  end
+    lowest_number
+    if number == lowest_number
+      return "#{name}"
+    end
+  end  
 end
